5d5cb2ad4a3049323c84ff5baaf695765ae65362
ehsan-0801 <ehsanulahmmed@gmail.com>
2022-05-27 20:34:33 +0600

    cancel order api done

--- index.js
+++ index.js
@@ -124,6 +124,12 @@ async function run() {
             const result = await orderCollection.insertOne(orders);
             return res.send({ success: true, result });
         });
+        app.delete('/order/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(filter);
+            res.send(result);
+        })
         app.get('/allorder', async (req, res) => {
             const orders = await orderCollection.find().toArray();
             res.send(orders);
